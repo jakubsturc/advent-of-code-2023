7d37e126d834dc88aacb29f54b98daafd50c24a9
Jakub Šturc <jakub.sturc@thermofisher.com>
2023-12-03 18:59:38 +0100

    add day03 part2 plus refactoring

--- day03/day03.go
+++ day03/day03.go
@@ -5,110 +5,168 @@ import (
 	"strconv"
 )
 
+type symbol struct {
+	x, y int
+	c    rune
+}
+
+type number struct {
+	sx, ex, yy int
+	val        int
+}
+
+type gear struct {
+	x, y int
+	nums []number
+}
+
+type plan struct{ lines []string }
+
 func Part1(path string) (int, error) {
 	lines, err := shared.ReadAllLines(path)
 	if err != nil {
 		return 0, err
 	}
-	parts := getPartNumbers(lines)
-	sum := sum(parts)
+
+	// get numbers and symbols
+	var p = plan{lines}
+	numbers := p.getNumbers()
+	symbols := p.getSymbols()
+
+	// filter parts
+	var parts []number
+	for _, number := range numbers {
+		for _, symbol := range symbols {
+			if isNextTo(number, symbol) {
+				parts = append(parts, number)
+			}
+		}
+	}
+
+	// sum part numbers
+	var sum = 0
+	for _, part := range parts {
+		sum += part.val
+	}
+
 	return sum, nil
 }
 
 func Part2(path string) (int, error) {
-	return 0, nil
-}
+	lines, err := shared.ReadAllLines(path)
+	if err != nil {
+		return 0, err
+	}
 
-func getPartNumbers(lines []string) []int {
-	maxX := len(lines[0])
-	maxY := len(lines)
-	parts := make([]int, 0)
-	var current string = ""
-	var isNextToSymbol bool = false
-	for y := 0; y < maxY; y++ {
-		for x := 0; x < maxX; x++ {
-			c := lines[y][x]
-			if '0' <= c && c <= '9' {
-				current += string(c)
-				isNextToSymbol = isNextToSymbol || hasSymbolNeighbour(lines, x, y)
-				continue
-			}
+	// get numbers and symbols
+	var p = plan{lines}
+	numbers := p.getNumbers()
+	symbols := p.getSymbols()
 
-			if current != "" {
-				if isNextToSymbol {
-					part, err := strconv.Atoi(current)
-					if err != nil {
-						panic(err)
-					}
-					parts = append(parts, part)
-				}
-				current = ""
-				isNextToSymbol = false
+	// filter gears
+	var gears []gear
+	for _, symbol := range symbols {
+		if isStar(symbol.c) {
+			gears = append(gears, gear{symbol.x, symbol.y, nil})
+		}
+	}
+
+	// associate gears with numbers
+	for i, gear := range gears {
+		for _, number := range numbers {
+			if isNextTo(number, symbol{gear.x, gear.y, '*'}) {
+				gears[i].add(number)
 			}
 		}
+	}
+
+	// sum filter correct gears and sum their ratios
+	var sum = 0
+	for _, gear := range gears {
+		if len(gear.nums) == 2 {
+			sum += gear.nums[0].val * gear.nums[1].val
+		}
+	}
+
+	return sum, nil
+}
+
+func isNextTo(n number, s symbol) bool {
+	return n.yy-1 <= s.y &&
+		s.y <= n.yy+1 &&
+		n.sx-1 <= s.x &&
+		s.x <= n.ex+1
+}
+
+func (g *gear) add(n number) {
+	g.nums = append(g.nums, n)
+}
+
+func (p *plan) getSymbols() []symbol {
+	var symbols []symbol
+	for y, line := range p.lines {
+		for x, c := range line {
+			if isSymbol(c) {
+				symbols = append(symbols, symbol{x, y, c})
+			}
+		}
+	}
+	return symbols
+}
+
+func (p *plan) getNumbers() []number {
+	var numbers []number
+	var current = ""
+	var sx, ex, yy = 0, 0, 0
+
+	yield := func() {
 		if current != "" {
-			if isNextToSymbol {
-				part, err := strconv.Atoi(current)
-				if err != nil {
-					panic(err)
-				}
-				parts = append(parts, part)
+			num, err := strconv.Atoi(current)
+			if err != nil {
+				panic(err)
 			}
+			number := number{sx, ex, yy, num}
+			numbers = append(numbers, number)
 			current = ""
-			isNextToSymbol = false
 		}
 	}
-	return parts
-}
 
-func hasSymbolNeighbour(lines []string, x, y int) bool {
-	neighbours := getNeighbours(lines, x, y)
-	for _, neighbour := range neighbours {
-		if isSymbol(neighbour) {
-			return true
-		}
-	}
-	return false
-}
-
-func getNeighbours(lines []string, x, y int) []rune {
-	var neighbours []rune
-	maxX := len(lines[0])
-	maxY := len(lines)
-	for i := -1; i <= 1; i++ {
-		for j := -1; j <= 1; j++ {
-			if i == 0 && j == 0 {
+	for y, line := range p.lines {
+		for x, c := range line {
+			if isDigit(c) {
+				if current == "" {
+					sx = x
+					yy = y
+				}
+				ex = x
+				current += string(c)
 				continue
 			}
-			xx := x + j
-			yy := y + i
-			if xx < 0 || xx >= maxX || yy < 0 || yy >= maxY {
-				continue
-			}
-			neighbours = append(neighbours, rune(lines[yy][xx]))
+			yield()
 		}
+		yield()
 	}
-	return neighbours
+	return numbers
 }
 
-func isNumber(c rune) bool {
+func isStar(c rune) bool {
+	return c == '*'
+}
+
+func isDigit(c rune) bool {
 	return '0' <= c && c <= '9'
 }
 
+func isDot(c rune) bool {
+	return c == '.'
+}
+
 func isSymbol(c rune) bool {
-	if c == '.' {
+	if isDot(c) {
 		return false
 	}
-	if isNumber(c) {
+	if isDigit(c) {
 		return false
 	}
 	return true
-}
-
-func sum(nums []int) int {
-	sum := 0
-	for _, num := range nums {
-		sum += num
-	}
-	return sum
 }
--- day03/day03_test.go
+++ day03/day03_test.go
@@ -25,3 +25,25 @@ func TestPart1(t *testing.T) {
 		t.Errorf("Expected 526404, got %d", result)
 	}
 }
+
+func TestPart2Example1(t *testing.T) {
+	result, err := Part2("./example1.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if result != 467835 {
+		t.Errorf("Expected 467835, got %d", result)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	result, err := Part2("./input.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if result != 84399773 {
+		t.Errorf("Expected 84399773, got %d", result)
+	}
+}
